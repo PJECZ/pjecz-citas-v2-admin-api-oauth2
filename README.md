3b9a0d6ac283eab8b14fac15ab58578982685cc8
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-07-20 07:25:07 -0500

    Avance en modelos basicos

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pjecz-citas-v2-admin-api-oauth2
-API OAuth2 del sistema de citas para brindar informacion a otros sistemas.
+
+API OAuth2 del Sistema de Citas V2 para brindar informacion a otros sistemas.
---
+++ citas_admin/v2/modulos/models.py
@@ -0,0 +1,29 @@
+"""
+Modulos v2, modelos
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from lib.database import Base
+from lib.universal_mixin import UniversalMixin
+
+
+class Modulo(Base, UniversalMixin):
+    """Modulo"""
+
+    # Nombre de la tabla
+    __tablename__ = "modulos"
+
+    # Clave primaria
+    id = Column(Integer, primary_key=True)
+
+    # Columnas
+    nombre = Column(String(256), unique=True, nullable=False)
+    nombre_corto = Column(String(64), nullable=False)
+
+    # Hijos
+    permisos = relationship("Permiso", back_populates="modulo")
+
+    def __repr__(self):
+        """Representación"""
+        return f"<Modulo {self.nombre}>"
