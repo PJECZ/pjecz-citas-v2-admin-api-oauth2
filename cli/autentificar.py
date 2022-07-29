52713928e75f551e6b73c2f34364bf6ffe8dbb35
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-07-29 09:26:02 -0500

    Cambio radical en estructura de directorios en el CLI

--- cli/autentificar.py
+++ cli/autentificar.py
@@ -7,27 +7,29 @@ from dotenv import load_dotenv
 import requests
 
 load_dotenv()
-HOST = os.getenv("HOST", "")
-PORT = os.getenv("PORT", "")
+HOST = os.getenv("HOST", "http://127.0.0.1:8006")
 USERNAME = os.getenv("USERNAME", "")
 PASSWORD = os.getenv("PASSWORD", "")
 
 
-def autentificar():
+def autentificar() -> dict:
     """Autentificarse"""
     if HOST == "":
         raise Exception("No se ha definido el host")
-    if PORT == "":
-        raise Exception("No se ha definido el puerto")
     if USERNAME == "":
         raise Exception("No se ha definido el usuario")
     if PASSWORD == "":
         raise Exception("No se ha definido la contraseña")
     data = {"username": USERNAME, "password": PASSWORD}
     headers = {"content-type": "application/x-www-form-urlencoded"}
-    response = requests.post(f"{HOST}:{PORT}/token", data=data, headers=headers)
+    response = requests.post(f"{HOST}/token", data=data, headers=headers)
     if response.status_code != 200:
         raise requests.HTTPError(response.text)
     token = response.json()["access_token"]
     authorization_header = {"Authorization": "Bearer " + token}
     return authorization_header
+
+
+def base_url() -> str:
+    """URL base de la API"""
+    return f"{HOST}/v2"
---
+++ cli/cit_citas.py
@@ -0,0 +1,52 @@
+"""
+CLI Cit Citas
+"""
+import requests
+import typer
+import rich
+
+import autentificar
+
+app = typer.Typer()
+
+
+def get_cit_citas(base_url: str, authorization_header: dict) -> dict:
+    """Consultar citas"""
+    parametros = {"limit": 10}
+    try:
+        response = requests.get(
+            f"{base_url}/cit_citas",
+            headers=authorization_header,
+            params=parametros,
+            timeout=12,
+        )
+    except requests.exceptions.RequestException as error:
+        raise error
+    if response.status_code != 200:
+        raise requests.HTTPError(response.status_code)
+    data_json = response.json()
+    if "items" not in data_json or "total" not in data_json:
+        raise ValueError("Error porque la respuesta de la API no es correcta")
+    return data_json
+
+
+@app.command()
+def exportar():
+    """Exportar"""
+    print("Exportar")
+
+
+@app.command()
+def ver():
+    """Ver"""
+    print("Ver")
+    rich.print(
+        get_cit_citas(
+            base_url=autentificar.base_url(),
+            authorization_header=autentificar.autentificar(),
+        )
+    )
+
+
+if __name__ == "__main__":
+    app()
