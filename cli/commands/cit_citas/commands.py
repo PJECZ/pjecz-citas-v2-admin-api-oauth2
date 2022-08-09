858a40b53835b30c0b43c4ba49002cbc86f92375
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-08-09 16:36:24 -0500

    Funciona el envio de mensajes con citas por oficina y fecha

--- cli/commands/cit_citas/commands.py
+++ cli/commands/cit_citas/commands.py
@@ -2,10 +2,16 @@
 Cit Citas Commands
 """
 from datetime import datetime
+import locale
+import os
 
+from dotenv import load_dotenv
 import pandas as pd
-import typer
 import rich
+import sendgrid
+from sendgrid.helpers.mail import Attachment, ContentId, Disposition, Email, FileContent, FileName, FileType, To, Content, Mail
+from tabulate import tabulate
+import typer
 
 import lib.connections
 import lib.exceptions
@@ -14,10 +20,17 @@ from .crud import get_cit_citas, get_cit_citas_cantidades_creados_por_dia, get_c
 
 app = typer.Typer()
 
+locale.setlocale(locale.LC_TIME, "es_MX.utf8")
+
 # Pandas options on how to display dataframes
 pd.set_option("display.max_rows", 500)
 pd.set_option("display.max_columns", 500)
 pd.set_option("display.width", 150)
+
+# SendGrid environment variables
+load_dotenv()
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+SENDGRID_FROM_EMAIL = os.getenv("SENDGRID_FROM_EMAIL")
 
 
 @app.command()
@@ -63,13 +76,73 @@ def consultar(
 
 @app.command()
 def enviar(
-    limit: int = 40,
-    fecha: str = None,
-    email: str = None,
-    oficina_clave: str = None,
+    email: str,
+    fecha: str,
+    oficina_clave: str,
+    estado: str = "PENDIENTE",
+    limit: int = 400,
 ):
     """Enviar mensaje con citas"""
     rich.print("Enviar mensaje con citas...")
+    # Validate sendgrid environment variables
+    try:
+        if SENDGRID_API_KEY is None or SENDGRID_API_KEY == "":
+            raise lib.exceptions.CLIConfigurationError("Falta SENDGRID_API_KEY")
+        if SENDGRID_FROM_EMAIL is None or SENDGRID_FROM_EMAIL == "":
+            raise lib.exceptions.CLIConfigurationError("Falta SENDGRID_FROM_EMAIL")
+    except lib.exceptions.CLIAnyError as error:
+        typer.secho(str(error), fg=typer.colors.RED)
+        raise typer.Exit()
+    # Get data
+    try:
+        respuesta = get_cit_citas(
+            base_url=lib.connections.base_url(),
+            authorization_header=lib.connections.authorization(),
+            limit=limit,
+            fecha=fecha,
+            oficina_clave=oficina_clave,
+            estado=estado,
+        )
+    except lib.exceptions.CLIAnyError as error:
+        typer.secho(str(error), fg=typer.colors.RED)
+        raise typer.Exit()
+    # Terminate if no data
+    if respuesta["total"] == 0:
+        typer.secho("No hay citas para enviar", fg=typer.colors.YELLOW)
+        raise typer.Exit()
+    # Convert data to HTML table
+    headers = ["id", "inicio", "nombre", "servicio", "notas"]
+    rows = []
+    for item in respuesta["items"]:
+        inicio = datetime.strptime(item["inicio"], "%Y-%m-%dT%H:%M:%S")
+        rows.append(
+            [
+                item["id"],
+                inicio.strftime("%H:%M"),
+                item["cit_cliente_nombre"],
+                item["cit_servicio_clave"],
+                item["notas"],
+            ]
+        )
+    table_html = tabulate(rows, headers=headers, tablefmt="html")
+    # Create message
+    subject = f"Citas de la oficina {oficina_clave} para la fecha {fecha}"
+    elaboracion_fecha_hora_str = datetime.now().strftime("%d/%B/%Y %I:%M%p")
+    contenidos = []
+    contenidos.append("<h1>PJECZ Citas V2</h1>")
+    contenidos.append(f"<h2>{subject}</h2>")
+    contenidos.append(table_html)
+    contenidos.append(f"<p>Fecha de elaboración: <b>{elaboracion_fecha_hora_str}.</b></p>")
+    contenidos.append("<p>ESTE MENSAJE ES ELABORADO POR UN PROGRAMA. FAVOR DE NO RESPONDER.</p>")
+    # Send message
+    sendgrid_client = sendgrid.SendGridAPIClient(api_key=SENDGRID_API_KEY)
+    from_email = Email(SENDGRID_FROM_EMAIL)
+    to_email = To(email)
+    content = Content("text/html", "<br>".join(contenidos))
+    mail = Mail(from_email, to_email, subject, content)
+    sendgrid_client.client.mail.send.post(request_body=mail.get())
+    # Print message
+    rich.print(f"Mensaje enviado a [blue]{email}[/blue] con [green]{respuesta['total']}[/green] citas")
 
 
 @app.command()
