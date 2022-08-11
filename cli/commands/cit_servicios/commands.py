7a2a8bd6c86e38174fa14e6986c6b79e95ce69a8
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-08-11 16:02:24 -0500

    Nuevas columnas de cit servicios

--- cli/commands/cit_servicios/commands.py
+++ cli/commands/cit_servicios/commands.py
@@ -28,13 +28,16 @@ def consultar(
         typer.secho(str(error), fg=typer.colors.RED)
         raise typer.Exit()
     console = rich.console.Console()
-    table = rich.table.Table("id", "clave", "descripcion", "duracion")
+    table = rich.table.Table("ID", "Clave", "Descripcion", "Duracion", "Desde", "Hasta", "Dias Habilitados")
     for registro in respuesta["items"]:
         table.add_row(
             str(registro["id"]),
             registro["clave"],
             registro["descripcion"],
             registro["duracion"],
+            str(registro["desde"]) if registro["desde"] is not None else "",
+            str(registro["hasta"]) if registro["hasta"] is not None else "",
+            registro["dias_habilitados"],
         )
     console.print(table)
     rich.print(f"Total: [green]{respuesta['total']}[/green] servicios")
